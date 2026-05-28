2974ea71de144ac8d05c827e86d95032d26e3822
Thenmozhi <kamarajthenmozhi0705@gmail.com>
2026-05-28 16:16:04 +0530

    Added SQL analysis query for BigQuery data

--- task2_pipeline_building/sample_query.sql
+++ task2_pipeline_building/sample_query.sql
@@ -1,1 +1,5 @@
-
+SELECT
+    AVG(temperature) AS average_temperature,
+    MAX(windspeed) AS highest_wind_speed,
+    COUNT(*) AS total_records
+FROM `weather-data-pipeline-497706.weather_pipeline.current_weather_data`;
